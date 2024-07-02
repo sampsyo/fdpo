5640c6325c98eedc0913b61d09beeb5be20a0c83
Adrian Sampson <adrian@radbox.org>
2024-07-02 12:42:50 -0400

    Execute via SMT

--- fdpo/__main__.py
+++ fdpo/__main__.py
@@ -1,7 +1,12 @@
 from .lang import parse
 from .check import check, CheckError
-from .smt import to_smt
+from .smt import to_smt, run
 import sys
+
+
+def parse_inputs(args: list[str]) -> dict[str, int]:
+    """Parse a list of arguments like a=5 b=42."""
+    return {key: int(value) for key, value in (arg.split("=") for arg in args)}
 
 
 def main():
@@ -20,6 +25,10 @@ def main():
             print(prog.pretty())
         case "smt":
             print(to_smt(prog))
+        case "run":
+            inputs = parse_inputs(sys.argv[2:])
+            for key, value in run(prog, inputs).items():
+                print(key, "=", value)
         case _:
             print(f"error: unknown mode {mode}", file=sys.stderr)
             sys.exit(1)
--- fdpo/smt.py
+++ fdpo/smt.py
@@ -1,11 +1,25 @@
 from . import lang, lib
-from pysmt.shortcuts import Symbol, Equals, And, to_smtlib
+from pysmt.shortcuts import (
+    Solver,
+    Symbol,
+    Equals,
+    And,
+    to_smtlib,
+    BV,
+    get_model,
+    get_env,
+)
 from pysmt.typing import BVType
 from pysmt.fnode import FNode
+from pysmt.logics import QF_BV
 from itertools import chain
+import shutil
+
+SymbolEnv = dict[str, FNode]
+Env = dict[str, int]
 
 
-def expr_to_smt(ports: dict[str, FNode], expr: lang.Expression):
+def expr_to_smt(ports: SymbolEnv, expr: lang.Expression):
     if isinstance(expr, lang.Lookup):
         return ports[expr.var]
     elif isinstance(expr, lang.Call):
@@ -15,18 +29,54 @@ def expr_to_smt(ports: dict[str, FNode], expr: lang.Expression):
         assert False
 
 
-def to_formula(prog: lang.Program) -> FNode:
+def to_formula(prog: lang.Program) -> tuple[SymbolEnv, FNode]:
     ports = {
         port.name: Symbol(port.name, BVType(port.width))
         for port in chain(prog.inputs.values(), prog.outputs.values())
     }
-
     constraints = [
         Equals(ports[asgt.dest], expr_to_smt(ports, asgt.expr))
         for asgt in prog.assignments
     ]
-    return And(*constraints)
+    return ports, And(*constraints)
 
 
 def to_smt(prog: lang.Program) -> str:
     return to_smtlib(to_formula(prog))
+
+
+def model_vals(model) -> Env:
+    """Get the bit-vector values from a pysmt `Model`."""
+    return {key.symbol_name(): value.bv2nat() for key, value in model}
+
+
+def solver(name: str, debug: bool = False):
+    # Do a mysterious global-state dance for pysmt to register solvers for later use.
+    smt_env = get_env()
+
+    match name:
+        case "z3":
+            smt_env.factory.add_generic_solver(
+                "z3", [shutil.which("z3"), "-smt2", "-in"], [QF_BV]
+            )
+        case "boolector":
+            smt_env.factory.add_generic_solver(
+                "boolector", [shutil.which("boolector"), "--smt2"], [QF_BV]
+            )
+
+    options = {}
+    if debug:
+        options["debug_interaction"] = True
+    return Solver(name=name, solver_options=options)
+
+
+def run(prog: lang.Program, env: Env) -> Env:
+    with solver("z3", True):
+        ports, prog_f = to_formula(prog)
+        env_constraints = [
+            Equals(ports[var], BV(value, prog.inputs[var].width))
+            for var, value in env.items()
+        ]
+        phi = And(prog_f, *env_constraints)
+        model = get_model(phi)
+    return {k: v for k, v in model_vals(model).items() if k in prog.outputs}
