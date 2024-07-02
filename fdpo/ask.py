2b49a3e9ebca48d82381d207069341ffd615af94
Adrian Sampson <adrian@radbox.org>
2024-07-02 16:53:03 -0400

    Who even knows anymore

---
+++ fdpo/ask.py
@@ -0,0 +1,26 @@
+import asyncio
+from ollama import AsyncClient
+import tomllib
+
+PROMPT = """
+Please write a Python program to compute the sum of two numbers. Just print the
+code; no text is necessary.
+""".strip()
+
+
+async def interact(host, model):
+    client = AsyncClient(host=host)
+    async for part in await client.generate(
+        model=model, prompt=PROMPT, stream=True
+    ):
+        print(part["response"], end="", flush=True)
+
+
+def do_something():
+    with open("config.toml", "rb") as f:
+        config = tomllib.load(f)
+    asyncio.run(interact(config["host"], config["model"]))
+
+
+if __name__ == "__main__":
+    do_something()
