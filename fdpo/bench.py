58589555360aecc2a64a9f168b12d2c9f37089c1
Adrian Sampson <adrian@radbox.org>
2024-07-07 19:23:57 -0400

    Even more parallelism?

--- fdpo/bench.py
+++ fdpo/bench.py
@@ -11,6 +11,17 @@ def gen_inputs(ports: list[lang.Port]) -> Env:
     return {port.name: random.getrandbits(port.width) for port in ports}
 
 
+async def bench_run_exp(prog: lang.Program, asker: ask.Asker) -> bool:
+    # Generate a test vector, and get the golden output.
+    inputs = gen_inputs(list(prog.inputs.values()))
+    outputs = smt.run(prog, inputs)
+
+    # "Ask" to run the same program.
+    test_outputs = await asker.run(prog, inputs)
+
+    return outputs == test_outputs
+
+
 async def bench_run_one(filename: str, asker: ask.Asker, count: int) -> int:
     # Parse the program.
     with open(filename) as f:
@@ -18,16 +29,10 @@ async def bench_run_one(filename: str, asker: ask.Asker, count: int) -> int:
     prog, _ = lang.parse(src)
 
     score = 0
-    for _ in range(count):
-        # Generate a test vector, and get the golden output.
-        inputs = gen_inputs(list(prog.inputs.values()))
-        outputs = smt.run(prog, inputs)
-
-        # "Ask" to run the same program.
-        test_outputs = await asker.run(prog, inputs)
-
+    tasks = [bench_run_exp(prog, asker) for _ in range(count)]
+    for task in asyncio.as_completed(tasks):
         # Score one when we match.
-        score += outputs == test_outputs
+        score += await task
 
     return score
 
